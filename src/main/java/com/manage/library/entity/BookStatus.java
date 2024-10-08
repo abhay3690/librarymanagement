29f90a5fc059ce18144c24fb5437638e2fcbf5ef
Abhay S <abhaysuthar7777@gmail.com>
2024-10-08 18:08:37 +0530

    add the some functionality

--- src/main/java/com/manage/library/entity/BookStatus.java
+++ src/main/java/com/manage/library/entity/BookStatus.java
@@ -1,5 +1,7 @@
 package com.manage.library.entity;
 
+import org.springframework.web.bind.annotation.PatchMapping;
+
 public enum BookStatus {
     AVAILABLE,
     ISSUED
--- src/main/java/com/manage/library/payload/APIResponse.java
+++ src/main/java/com/manage/library/payload/APIResponse.java
@@ -4,13 +4,37 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
-@Data
+
 @Builder
 public class APIResponse<T> {
 
     private T data;
 
     private String message;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
 
     private HttpStatus status;
 
